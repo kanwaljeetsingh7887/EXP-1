6600ba622a2e1913c761225eb46297546dca476e
Kanwaljeet Singh <97405821+kanwaljeetsingh7887@users.noreply.github.com>
2024-08-01 23:14:17 +0530

    Add files via upload

---
+++ EXP-1B.cpp
@@ -0,0 +1,22 @@
+//KANWALJEET SINGH
+//23070123124
+//ENTC B2
+//EXP 1B
+#include<iostream>
+using namespace std;
+int main() {
+int n1, n2, sum=0, sub, prod, div;
+cout<<"Enter first number."; 
+cin>>n1;
+cout<<"Enter second number.";
+cin>>n2;
+sum=n1+n2;
+cout<<"Sum of the numbers is: "<<sum<<"\n"; 
+sub=n1-n2;
+cout<<"Difference of the numbers is: "<<sub<<"\n"; 
+prod=n1*n2;
+cout<<"Product of the numbers is: "<<prod<<"\n"; 
+div=n1/n2;
+cout<<"Quotient is: "<<div<<"\n";
+return 0;
+}
